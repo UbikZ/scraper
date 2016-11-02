4b7f457cc088eacdbe49ec755b3134292e4b1068
Gabriel Malet <gabrielmalet@gmail.com>
2016-11-02 01:25:05 +0100

    Change database on_ready detection

--- api/app/resource/feed/model.py
+++ api/app/resource/feed/model.py
@@ -1,10 +1,12 @@
-from sqlalchemy import Column, Integer, Sequence, String
+from sqlalchemy import Column, Integer, Sequence, String, Boolean
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
+
 class FeedModel(Base):
-    __tablename__ = 'feed_model'
+    __tablename__ = 'feed'
     id = Column(Integer, Sequence('feed_id_seq'), primary_key=True)
     label = Column(String, nullable=False)
     url = Column(String, nullable=False)
+    is_enabled = Column(Boolean, default=False)
--- api/scripts/entrypoint.sh
+++ api/scripts/entrypoint.sh
@@ -1,5 +1,37 @@
 #!/bin/bash
 
-while ! nc -z pgsql 5432; do sleep 1; done
+# Wait database is online
 
+echo "[DEBUG] > Check database availability"
+python - "$DB_CONN_STRING" <<END
+import sys, psycopg2, time
+from urlparse import urlparse
+
+maxTries = 10
+
+parsed = urlparse(sys.argv[1])
+user = parsed.username
+password = parsed.password
+host = parsed.hostname
+port = parsed.port
+database = parsed.path.strip('/')
+
+while True:
+    try:
+        if maxTries == 0:
+            sys.exit(0)
+        conn = psycopg2.connect(host=host, port=port, user=user, password=password, database=database)
+        break
+    except:
+        --maxTries
+        time.sleep(1)
+END
+echo "[DEBUG] > Database is ready."
+
+# Apply migrations if needed
+echo "[DEBUG] > Apply migrations."
+alembic upgrade head
+
+# Launch server
+echo "[DEBUG] > Launch server."
 gunicorn --reload -c /src/gunicorn/config.py app:api
